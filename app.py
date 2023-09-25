0312b234c5352184031da61cd2767697528e793d
Michael Wood <michaelwood33311@icloud.com>
2023-09-25 18:44:03 -0400

    added code examples

--- app.py
+++ app.py
@@ -20,3 +20,14 @@ st.markdown("""
             - b2
             - b3
             """)
+
+st.markdown("That is a joyful emoji :joy:")
+
+json={"a": [1, 2, 3], "b": [4, 5, 6]}
+st.json(json)
+
+st.code("""
+print("hello World")
+x = 5
+print("x", x)
+""", language="python")

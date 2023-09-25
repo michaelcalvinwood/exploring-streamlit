8ff7a5131c4719c0602d2b4dcfaa21b09bff0a7b
Michael Wood <michaelwood33311@icloud.com>
2023-09-25 18:36:57 -0400

    added markdown examples

--- app.py
+++ app.py
@@ -4,3 +4,19 @@ st.title("I am a Title")
 st.header("I am a Header")
 st.subheader("I am a Subheading")
 st.text("I am text.")
+
+st.markdown("### I am *Markdown*.")
+st.markdown(">This is a markdown block quote.")
+st.markdown(">>This is a nested markdown block quote.")
+
+st.markdown("[Google](https://google.com)")
+
+st.markdown("""
+            1. One
+            2. Two
+            3. Three""")
+st.markdown("""
+            - b1
+            - b2
+            - b3
+            """)

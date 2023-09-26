38e5075431c011f86591a3ad7888aea343995f5a
Michael Wood <michaelwood33311@icloud.com>
2023-09-26 07:39:18 -0400

    added choices components (radio, checkbox, select, multi-select) code snippets

--- app.py
+++ app.py
@@ -1,16 +1,37 @@
+# https://www.youtube.com/watch?v=oTbbfuH44Gg&list=PLa6CNrvKM5QU7AjAS90zCMIwi9RTFNIIW&index=8
+
 import streamlit as st
-import pandas as pd
 
-st.metric(label="Wind Speed", value="120ms⁻¹", delta="-1.4ms⁻¹") # use \^ tab
+test_me = "US"
+def cats_clicked():
+    print('cats state', st.session_state["cats"])
 
-dict = {
-    "column 1": [1,2,3,4,5,6],
-    "column 2": [2,4,6,8,10,12],
-    "column 3": [3,6,9,12,15,18]
-}
-df = pd.DataFrame(dict)
-st.table(df) # allows you to view tables in full screen
-st.dataframe(df) # allows you sort columns
+def radio_clicked():
+    print("radio", st.session_state["radio"])
+
+def buttonClicked():
+    global test_me
+    print("button clicked")
+    test_me = "hello"
+
+def selected_car():
+    print("selected car", st.session_state["favorite_car"])
+
+def selected_car2():
+    print("selected car2", st.session_state["favorite_car2"])
 
 
+st.checkbox("Cats", key="cats", on_change=cats_clicked)
 
+radio = st.radio("Select One", options=("US", "UK", "Canada"), key="radio", on_change=radio_clicked)
+st.button("Click Me", on_click=buttonClicked)
+
+select=st.selectbox("What is your favorite car?",
+                    options=("Audi", "BMW", "Mercedes", "Toyota"),
+                    key="favorite_car",
+                    on_change=selected_car)
+
+multi_select=st.multiselect("What is your favorite car?",
+                    options=("Audi", "BMW", "Mercedes", "Toyota"),
+                    key="favorite_car2",
+                    on_change=selected_car2)
---
+++ media.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.image("flowing-money.jpg",caption="Making Money Flow")
+
+st.image("flowing-money.jpg",caption="Making Money Flow", width=250)
+
+st.audio("test.wav")
+
+st.video("Contributor-Pages.mp4")

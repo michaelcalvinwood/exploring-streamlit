c333063c22d02b6ccee34948e018affd07c308fe
Michael Wood <michaelwood33311@icloud.com>
2023-10-04 13:19:41 -0400

    added progress bar code snippets

--- date-time-input.py
+++ date-time-input.py
@@ -1,5 +1,14 @@
 import streamlit as st
+from datetime import time
+import time as ts
 
 birthday = st.date_input("My birthday")
 
-birth_time = st.time_input("I was born at")
+timer = st.time_input("Timer", value=time(0, 0, 0)) # set default value 0 hours, 0 minutes, 0 seconds
+
+# timer is a datetime object
+if str(timer) == "00:00:00":
+    st.write("Please set the timer.")
+else:
+    st.write("Performing timer countdown")
+    
